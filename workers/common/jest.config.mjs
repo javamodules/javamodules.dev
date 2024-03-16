d8a8346f08ad8950216274b487b2eb9a4b6e4da7
Sam Gammon <sam@elide.ventures>
2024-03-16 16:08:57 -0700

    chore: ci process, testing, coverage, test reporting
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- workers/common/jest.config.mjs
+++ workers/common/jest.config.mjs
@@ -3,4 +3,11 @@ export default {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {},
+  reporters: [
+    "default",
+    ["jest-junit", {
+      suiteName: 'Site Frontend',
+      outputDirectory: 'reports',
+    }]
+  ],
 };
--- workers/graph/jest.config.mjs
+++ workers/graph/jest.config.mjs
@@ -2,4 +2,11 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  reporters: [
+    "default",
+    ["jest-junit", {
+      suiteName: 'Site Frontend',
+      outputDirectory: 'reports',
+    }]
+  ],
 };
